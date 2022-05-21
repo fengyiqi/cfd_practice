801006012c0b85ea2669c1baf9e596c4c5a68b4c
fengyiqi <yiqi.feng@hotmail.com>
2022-05-21 11:07:56 +0200

    delete comments

--- jupyter/06_Inviscid_Burgers_CRWENO/crweno5_scheme.py
+++ jupyter/06_Inviscid_Burgers_CRWENO/crweno5_scheme.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 epsilon = 1e-6
 
 weights_0 = 0.2
@@ -27,16 +25,4 @@ def crweno5_nonlinear_weights(value):
     b1, b2, b3 = w0 / 6, (5 * w0 + 5 * w1 + w2) / 6, (w1 + 5 * w2) / 6
     
     return a1, a2, a3, b1, b2, b3
-
-
-# def crweno5_reconstruction(value):
-#     v0, v1, v2, v3, v4 = value[0], value[1], value[2], value[3], value[4]
-#
-#     a1, a2, a3, b1, b2, b3 = crweno5_nonlinear_weights(value)
-#
-#     term0 = w0 * (1/3 * v0 - 7/6 * v1 + 11/6 * v2)
-#     term1 = w1 * (-1/6 * v1 + 5/6 * v2 + 1/3 * v3)
-#     term2 = w2 * (1/3 * v2 + 5/6 * v3 - 1/6 *v4)
-#
-#     return term0 + term1 + term2
-    
+  

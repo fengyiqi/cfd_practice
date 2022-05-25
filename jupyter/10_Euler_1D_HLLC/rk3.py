354a1e4bc7c7b1a8525582e3aa40bc289d153c0a
yiqi <yiqi.feng@tum.de>
2022-05-25 15:40:16 +0200

    reformate

--- jupyter/10_Euler_1D_HLLC/rk3.py
+++ jupyter/10_Euler_1D_HLLC/rk3.py
@@ -1,10 +1,11 @@
 import numpy as np
 
+coef = np.array([
+    (1, 0, 1),
+    (0.75, 0.25, 0.25),
+    (1/3, 2/3, 2/3)
+])
+
 def rk3(stage, u, rhs, dt, temp=None):
-    if stage == 1:
-        return u + dt * rhs
-    if stage == 2:
-        return 0.75 * u + 0.25 * temp + 0.25 * dt * rhs
-    if stage == 3:
-        return (1/3) * u + (2/3) * temp + (2/3) * dt * rhs
+    return coef[stage-1][0] * u + coef[stage-1][1] * temp + coef[stage-1][2] * dt * rhs
     
